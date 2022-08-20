9df8a58f6e1edc796b20155dbc3b7fac6ff4c712
a345163745 <0oalexlio0@gmail.com>
2022-08-19 23:07:47 -0400

    new fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from "react"
+import "./style.css"
+import axios from "axios"
+const options = {
+    method: 'GET',
+    url: 'https://dad-jokes.p.rapidapi.com/random/joke',
+    headers: {
+      'X-RapidAPI-Key': '463a485356msh669da8755eb8bdbp18cda9jsn4a5b2c27908b',
+      'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
+    }
+  };
+export default function App(){
+    const [joke,setJoke] = useState("")
+    const [punchLine, setPunchLine] = useState("")
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const GetJoke = async ()=>{
+        /* This is the regular promise way using then and catch
+        axios.request(options).then(function (response) {
+            setJoke(response.data.body[0].setup);
+            setPunchLine(response.data.body[0].punchline);
+        }).catch(function (error) {
+            console.error(error);
+        });*/
+        /* This is the most clean way using async write readable code and not nested*/
+        const res = await axios.request(options).catch(err => console.error(err));
+        
+        const joke = await res.data.body[0].setup
+        const punchline = await res.data.body[0].punchline
+        setJoke(joke);
+        setPunchLine(punchline);
+        
+    }
+    return (
+        <> 
+        {` ${joke} -> ${punchLine} `}
+        <button onClick={GetJoke}>Get a New Joke</button>
+        </>
+    )
 }
-
-export default App;
